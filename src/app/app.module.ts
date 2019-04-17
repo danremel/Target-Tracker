2951a9e694c2b8e203bff9e6f3138f13bd9e8c0e
danremel <remel.dan@gmail.com>
2019-04-17 12:14:35 -0400

    Added main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TargetFormComponent } from './target-form/target-form.component';
+import { TargetsComponent } from './targets/targets.component';
+import { TargetComponent } from './targets/target/target.component';
+import { TargetDetailsComponent } from './targets/target-details/target-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TargetFormComponent,
+    TargetsComponent,
+    TargetComponent,
+    TargetDetailsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/target-form/target-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-target-form',
+  templateUrl: './target-form.component.html',
+  styleUrls: ['./target-form.component.css']
+})
+export class TargetFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
