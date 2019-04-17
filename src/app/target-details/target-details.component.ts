d5de7cc58bffbdd36bc39b4d42f93f1ad5695175
danremel <remel.dan@gmail.com>
2019-04-17 17:24:53 -0400

    target-details now subscribes to the target service to pass in the selected target data into the component

--- src/app/target-details/target-details.component.ts
+++ src/app/target-details/target-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Target } from '../shared/target.model';
+import { TargetsService } from '../targets.service';
 
 @Component({
   selector: 'app-target-details',
@@ -9,7 +10,11 @@ import { Target } from '../shared/target.model';
 export class TargetDetailsComponent implements OnInit {
   @Input() target: Target;
 
-  constructor() { }
+  constructor(private targetsService: TargetsService) {
+    this.targetsService.targetSelected.subscribe(
+      (target: Target) => this.target = target
+    );
+  }
 
   ngOnInit() {
   }
