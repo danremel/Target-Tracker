2b99871167484c72f22142f12e11d5209972c836
danremel <remel.dan@gmail.com>
2019-04-17 12:34:45 -0400

    Added models for Target and Contact

---
+++ src/app/shared/target.model.ts
@@ -0,0 +1,13 @@
+export class Target {
+    constructor(
+        public compName: string,
+        public compAddress: string,
+        public dateFirstContact: string,
+        public daysSinceFirstContact: string,
+        public industry: string,
+        public website: string,
+        public type: string,
+        public revenue: string,
+        public status: string,
+    ) {}
+}
---
+++ src/app/targets/target/contacts.model.ts
@@ -0,0 +1,8 @@
+export class Contact {
+    constructor(
+        public name: string,
+        public phoneNumber: string,
+        public emailAddress: string,
+        public role: string
+    ) {}
+}
